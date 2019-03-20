b4416097223651540269918618439390f1047fa3
Louis Seven <45105357+louisdseven@users.noreply.github.com>
2019-03-20 11:56:43 -0400

    Added quiz app files

---
+++ datastore.js
@@ -0,0 +1,112 @@
+const STORE = [
+    {
+        question: 'What is the fastest land animal?',
+        answers: [
+            'Wolf',
+            'Tiger',
+            'Cheetah',
+            'Giraffe',
+            'Ostrich'
+        ],
+        correctAnswer: 'Cheetah'
+    },
+    {
+        question: 'Which of these is the fastest fish?',
+        answers: [
+            'Shark',
+            'Sailfish',
+            'Tuna',
+            'Flying Fish',
+            'Porgy'
+        ],
+        correctAnswer: 'Sailfish'
+    },
+    {
+        question: 'What is the fastest bird?',
+        answers: [
+            'Peregrine Falcon',
+            'Hawk',
+            'Eagle',
+            'Stork',
+            'Finch'
+        ],
+        correctAnswer: 'Peregrine Falcon'
+    },
+    {
+        question: 'Which of these is the shortest time span?',
+        answers:  [
+            'A dozen years',
+            'Avarage lifespan of a sea turtle',
+            'Century',
+            'Decade',
+            'Millenium'
+        ],
+        correctAnswer: 'Decade'
+    },
+    {
+        question: 'Which of these is the shortest measurement of length?',
+        answers: [
+            'Gram',
+            'Inch',
+            'Yard',
+            'Centimeter',
+            'Meter'
+        ],
+        correctAnswer: 'Centimeter'
+    },
+    {
+        question: 'Which of these words best completes the following sentence grammatically: He _______ dodged the speeding car.',
+        answers: [
+            'quickly',
+            'fastly',
+            'rapidly',
+            'either of these',
+            'neither of these'
+        ],
+        correctAnswer: 'quickly'
+    },
+    {
+        question: 'Which of these things most likely takes up the least time?',
+        answers: [
+            'Reading a novel with at least 300 pages',
+            'Watching a movie',
+            'Driving from Los Angeles to New York City',
+            'Running a 5k',
+            'Taking a shower'
+        ],
+        correctAnswer: 'Taking a shower'
+    },
+    {
+        question: 'What is the shortest sentence in the English language?',
+        answers: [
+            'Go!',
+            'A.',
+            'I am.',
+            'Yes.',
+            'None of these'
+        ],
+        correctAnswer: 'Go!'
+    },
+    {
+        question: 'Of the following, who was the shortest U.S. president?',
+        answers: [
+            'Thomas Jefferson',
+            'William Henry Harrison',
+            'George Washington',
+            'Abraham Lincoln',
+            'James Madison'
+        ],
+        correctAnswer: 'James Madison'
+    },
+    {
+        question: 'Which of the following U.S. presidents served the shortest amount of time?',
+        answers: [
+            'William Henry Harrison',
+            'George Washington',
+            'Abraham Lincoln',
+            'James Madison',
+            'Thomas Jefferson'
+        ],
+        correctAnswer: 'William Henry Harrison'
+    },
+];
---
+++ script.js
@@ -0,0 +1,151 @@
+let questionNumber = 0;
+let score = 0;
+
+//generate question html
+function generateQuestion () {
+  if (questionNumber < STORE.length) {
+    return `<div class="question-${questionNumber}">
+    <h2>${STORE[questionNumber].question}</h2>
+    <form>
+    <fieldset>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[0]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[1]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[2]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[3]}</span>
+    </label>
+    <label class="answerOption">
+    <input type="radio" value="${STORE[questionNumber].answers[4]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[4]}</span>
+    </label>
+    <button type="submit" class="submitButton">Submit</button>
+    </fieldset>
+    </form>
+    </div>`;
+} else {
+    renderResults();
+    restartQuiz();
+    $('.questionNumber').text(10)
+  }
+}
+
+//increment question number
+function changeQuestionNumber () {
+  //if (questionNumber < STORE.length) {
+    questionNumber ++;
+  //}
+  $('.questionNumber').text(questionNumber+1);
+}
+
+//increment score
+function changeScore () {
+  score ++;
+}
+
+//start quiz
+//on startQuizButton click hide start div
+//unhide quiz form div
+function startQuiz () {
+  $('.quizStart').on('click', '.startButton', function (event) {
+    $('.quizStart').remove();
+    $('.questionAnswerForm').css('display', 'block');
+    $('.questionNumber').text(1);
+});
+}
+
+// render question in DOM
+function renderQuestion () {
+  $('.questionAnswerForm').html(generateQuestion());
+}
+
+//user selects answer on submit run user feedback
+function userSelectAnswer () {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    let selected = $('input:checked');
+    let answer = selected.val();
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    if (answer === correctAnswer) {
+      selected.parent().addClass('correct');
+      ifAnswerIsCorrect();
+    } else {
+      selected.parent().addClass('wrong');
+      ifAnswerIsWrong();
+    }
+  });
+}
+
+function ifAnswerIsCorrect () {
+  userAnswerFeedbackCorrect();
+  updateScore();
+}
+
+function ifAnswerIsWrong () {
+  userAnswerFeedbackWrong();
+}
+
+//user feedback for correct answer
+function userAnswerFeedbackCorrect () {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><img src="correct.gif" alt="Trump says: Correct!"/></div><p><b>You got it right!</b></p><button type=button class="nextButton">Next</button></div>`);
+}
+
+//user feedback for wrong answer
+function userAnswerFeedbackWrong () {
+  let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+  // let iconImage = `${STORE[questionNumber].icon}`;
+  $('.questionAnswerForm').html(`<div class="correctFeedback"><div class="icon"><img src="wrong.gif" alt="Trump says: Wrong!"/></div><p><b>You got it wrong</b><br>the correct answer is <span>"${correctAnswer}"</span></p><button type=button class="nextButton">Next</button></div>`);
+}
+
+//update score text
+function updateScore () {
+  changeScore();
+  $('.score').text(score);
+}
+
+//when quiz is over this is the html for the page
+function renderResults () {
+  if (score >= 8) {
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>You're on target!</h3><img src="cat-attacking.gif" alt="cat attacking giphy"/><p>You got ${score} / 10</p><p>Wow!! Now we know who to call when we play trivia ;)</p><button class="restartButton">Restart Quiz</button></div>`);
+  } else if (score < 8 && score >= 5) {
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>Almost there!</h3><img src="cat-perplexed.gif" alt="perplexed cat giphy"/><p>You got ${score} / 10</p><p>That was not too shabby. Try again?</p><button class="restartButton">Restart Quiz</button></div>`);
+  } else {
+    $('.questionAnswerForm').html(`<div class="results correctFeedback"><h3>Seems like you need some googling to do</h3><img src="cat-falling.gif" alt="sleepy kitten giphy"/><p>You got ${score} / 10</p><p>Keep trying. Knowledge is power.</p><button class="restartButton">Restart Quiz</button></div>`);
+  }
+}
+
+//what happens when the user clicks next
+function renderNextQuestion () {
+  $('main').on('click', '.nextButton', function (event) {
+    changeQuestionNumber();
+    renderQuestion();
+    userSelectAnswer();
+  });
+}
+
+//restart quiz function - reloads page to start quiz over
+function restartQuiz () {
+  $('main').on('click', '.restartButton', function (event) {
+    location.reload();
+  });
+}
+
+//run quiz functions
+function createQuiz () {
+  startQuiz();
+  renderQuestion();
+  userSelectAnswer();
+  renderNextQuestion();
+}
+
+$(createQuiz);
